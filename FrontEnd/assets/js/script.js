72f6592a31ade8c503ca8c0c68acc959d27e9b6a
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2023-07-15 03:51:18 +0200

    début de suite

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,30 +1,39 @@
-let gallery = document.querySelector('.gallery')
-let projects = []
+// Modifiez le chemin vers l'API si nécessaire
+const apiUrl = 'http://localhost:5678/api/works';
 
-async function getProjects(){
-  await fetch ('http://localhost:5678/api/works')
-    .then((response)=>response.json())
-    .then((data)=>{
-      projects = data
-    })
-    .catch((error)=>console.log(error))
+let gallery = document.querySelector('.gallery');
+let projects = [];
 
+async function getProjects() {
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    projects = data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
 }
 
-async function displayProjects(){
-  await getProjects()
-  console.log(projects)  
-  for(let project of projects){
-    console.log(project)
-    let figure = document.createElement('figure')
-    figure.setAttribute('class','display')
+async function displayProjects() {
+  await getProjects();
 
-    let image = new Image();  // Créer un nouvel élément d'image
-    image.src = project.imageURL; // Définir la source de l'image
+  for (let project of projects) {
+    let figure = document.createElement('figure');
+    figure.setAttribute('class', 'display');
+
+    let image = new Image();
+    // Assurez-vous que la propriété "imageURL" est correctement définie dans l'objet "project"
+    // Utilisez la propriété "imageURL" pour définir la source de l'image
+    image.src = project.imageURL;
     image.alt = project.title;
     figure.appendChild(image);
 
     let caption = document.createElement('figcaption');
+    // Assurez-vous que la propriété "title" est correctement définie dans l'objet "project"
+    // Utilisez la propriété "title" pour définir le texte de la légende
     caption.innerText = project.title;
     figure.appendChild(caption);
 
