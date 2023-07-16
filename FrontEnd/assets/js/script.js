e9747dd8bec5ae8b2b05dc299a9b4477ef6949e3
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2023-07-17 00:47:38 +0200

    apparition des images en fetch

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,41 +1,39 @@
-// Modifiez le chemin vers l'API si nécessaire
-const apiUrl = 'http://localhost:5678/api/works';
-
+// JavaScript Code
 let gallery = document.querySelector('.gallery');
 let projects = [];
 
 async function getProjects() {
-  try {
-    const response = await fetch(apiUrl);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const data = await response.json();
-    projects = data;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
+  await fetch('http://localhost:5678/api/works')
+    .then((response) => response.json())
+    .then((data) => {
+      projects = data;
+    })
+    .catch((error) => console.log(error));
 }
 
 async function displayProjects() {
   await getProjects();
-
+  console.log(projects);
   for (let project of projects) {
+    console.log(project);
+
     let figure = document.createElement('figure');
     figure.setAttribute('class', 'display');
 
-    let image = new Image();
-    // Assurez-vous que la propriété "imageURL" est correctement définie dans l'objet "project"
-    // Utilisez la propriété "imageURL" pour définir la source de l'image
-    image.src = project.imageURL;
-    image.alt = project.title;
-    figure.appendChild(image);
+    // Add the image to the figure element
+    let img = document.createElement('img');
+    img.setAttribute('src', project.imageUrl); // Assuming imageUrl is the property holding the image URL
+    img.setAttribute('alt', project.title); // Assuming title is the property holding the image alt text
+    figure.appendChild(img);
 
-    let caption = document.createElement('figcaption');
-    // Assurez-vous que la propriété "title" est correctement définie dans l'objet "project"
-    // Utilisez la propriété "title" pour définir le texte de la légende
-    caption.innerText = project.title;
-    figure.appendChild(caption);
+    // Add project information (e.g., title and description) to the figure element
+    let titleElement = document.createElement('h2');
+    titleElement.textContent = project.title; // Assuming title is the property holding the project title
+    figure.appendChild(titleElement);
+
+    let descriptionElement = document.createElement('p');
+    descriptionElement.textContent = project.description; // Assuming description is the property holding the project description
+    figure.appendChild(descriptionElement);
 
     gallery.appendChild(figure);
   }
