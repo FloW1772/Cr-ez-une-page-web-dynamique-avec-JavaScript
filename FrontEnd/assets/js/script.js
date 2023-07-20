639d8357362952cf3cbacee15d8a358bc1a477b3
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2023-07-20 13:49:21 +0200

    trie par catégorie le code

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,14 +1,22 @@
-// JavaScript Code
 let gallery = document.querySelector('.gallery');
 let projects = [];
 let categories = [];
-let categoriesFilters = document.querySelector('.categories-filters')
+let categoriesFilters = document.querySelector('.categories-filters');
 
 async function getProjects() {
   await fetch('http://localhost:5678/api/works')
     .then((response) => response.json())
     .then((data) => {
       projects = data;
+    })
+    .catch((error) => console.log(error));
+}
+
+async function getCategories() {
+  await fetch('http://localhost:5678/api/categories')
+    .then((response) => response.json())
+    .then((data) => {
+      categories = data;
     })
     .catch((error) => console.log(error));
 }
@@ -21,7 +29,7 @@ async function displayProjects() {
 
     let figure = document.createElement('figure');
     figure.setAttribute('class', 'display');
-    figure.setAttribute('data-category-id',project.categoryId)
+    figure.setAttribute('data-category-id', project.categoryId);
 
     // Add the image to the figure element
     let img = document.createElement('img');
@@ -34,40 +42,36 @@ async function displayProjects() {
     titleElement.textContent = project.title; // Assuming title is the property holding the project title
     figure.appendChild(titleElement);
 
-
     gallery.appendChild(figure);
   }
 }
 
-displayProjects();
-
-async function getCategories() {
-  await fetch('http://localhost:5678/api/categories')
-    .then((response) => response.json())
-    .then((data) => {
-      categories = data;
-    })
-    .catch((error) => console.log(error));
-}
-async function displayCategories(){
-  await getProjects();
+async function displayCategories() {
   await getCategories();
   console.log(categories);
-  for (let categorie of categories){
-    console.log(categorie)
-    let button = document.createElement('button')
-    button.innerText = categorie.name
+  for (let category of categories) {
+    console.log(category);
+
+    let button = document.createElement('button');
+    button.innerText = category.name;
     categoriesFilters.appendChild(button);
-    button.addEventListener("click",function(event){
-      let projectsInGallery = document.querySelectorAll(".gallery figure")
-      for (let projectInGallery of projectsInGallery){
-        console.log(categorie.id)
-        console.log(projectInGallery.getAttribute('data-category-id'))
-        console.log('-----')
+
+    button.addEventListener('click', function (event) {
+      let projectsInGallery = document.querySelectorAll('.gallery figure');
+
+      for (let projectInGallery of projectsInGallery) {
+        console.log(category.id);
+        console.log(projectInGallery.getAttribute('data-category-id'));
+
+        if (parseInt(projectInGallery.getAttribute('data-category-id')) === category.id) {
+          projectInGallery.style.display = 'block'; // Afficher les projets de la catégorie sélectionnée
+        } else {
+          projectInGallery.style.display = 'none'; // Masquer les projets qui n'appartiennent pas à la catégorie sélectionnée
+        }
       }
-    })
-
-
+    });
   }
 }
+
+displayProjects();
 displayCategories();
