c8c434300165ede563e874262acc0cf13616c56d
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2023-07-22 04:56:06 +0200

    v2 du js du login

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,62 +1,49 @@
-// main.js
-const url = 'https://your-api-url/users/login';
-
-document.getElementById('loginForm').addEventListener('submit', (event) => {
-  event.preventDefault(); // Prevent form submission
-
-  // Get the form values
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-
-  // Form validation
-  if (!email || !password) {
-    console.error('Tous les champs sont obligatoires.');
-    return;
-  }
-
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(email)) {
-    console.error('L\'email n\'est pas valide.');
-    return;
-  }
-
-  // Données d'identification de l'utilisateur
-  const payload = {
-    email: email,
-    password: password
-  };
-
-  // Options de la requête POST
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(payload)
-  };
-
-  // Envoi de la requête POST
-  fetch(url, options)
-  .then(response => {
-    if (response.ok) {
-      return response.json()
+// Fonction pour effectuer la connexion (simulée ici)
+function performLogin(username, password) {
+  // Remplacez cette partie par votre logique de connexion réelle
+  return new Promise((resolve, reject) => {
+    // Supposons que le nom d'utilisateur est "admin" et le mot de passe est "12345"
+    if (username === "admin" && password === "12345") {
+      resolve({ message: "Connexion réussie !" });
     } else {
-      throw new Error('La connexion a échoué.')
+      reject({ message: "Nom d'utilisateur ou mot de passe incorrect." });
     }
-  })
-  .then(data => {
-    // La connexion a réussi
-    const userId = data.userId
-    const token = data.token
+  });
+}
 
-    // Enregistrement du token en local storage au lieu de session storage
-    localStorage.setItem('userId', userId)
-    localStorage.setItem('token', token)
+// Fonction pour stocker le token dans le localStorage
+function storeToken(token) {
+  localStorage.setItem("userToken", token);
+}
 
-    // Redirection vers la page d'accueil (modifier le chemin en fonction de votre application)
-    window.location.href = '/home'
-  })
-  .catch(error => {
-    console.error(error)
-  })
-  })
+// Sélectionner le formulaire et ajouter un gestionnaire d'événement pour la soumission
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // Empêche l'envoi du formulaire par défaut
+
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  // Appeler la fonction de connexion avec les informations d'identification
+  performLogin(username, password)
+    .then((response) => {
+      alert(response.message); // Connexion réussie, affichez un message approprié
+      
+      // Supposons que vous avez un jeton d'authentification généré par votre serveur
+      const authToken = "votre_token_d_authentification"; // Remplacez-le par votre jeton réel
+
+      // Stocker le token dans le localStorage
+      storeToken(authToken);
+
+      // Vous pouvez rediriger l'utilisateur vers une autre page ici
+      // window.location.href = "page_de_destination.html";
+    })
+    .catch((error) => {
+      alert(error.message); // Affichez un message d'erreur en cas d'échec de connexion
+    });
+});
+
+// Récupérer le token du localStorage (peut être fait à n'importe quel moment dans votre script)
+const storedToken = localStorage.getItem("userToken");
+console.log(storedToken); // Vous pouvez utiliser le token récupéré pour vos actions ultérieures
