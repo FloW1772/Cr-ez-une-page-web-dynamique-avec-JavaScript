f467e4b1e9bd68eed995a01bedfa80a76375ec55
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2023-07-28 02:39:49 +0200

    début de mise en place de la modale

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,20 +1,18 @@
-// Fonction pour effectuer la connexion (simulée ici)
-function performLogin(username, password) {
-  // Remplacez cette partie par votre logique de connexion réelle
-  return new Promise((resolve, reject) => {
-    // Supposons que le nom d'utilisateur est "admin" et le mot de passe est "12345"
-    if (email === "admin" && password === "12345") {
-      resolve({ message: "Connexion réussie !" });
-    } else {
-      reject({ message: "Nom d'utilisateur ou mot de passe incorrect." });
-    }
-  });
-}
+// // Fonction pour effectuer la connexion (simulée ici)
+// function performLogin(username, password) {
+//   // Remplacez cette partie par votre logique de connexion réelle
+//   return new Promise((resolve, reject) => {
+//     // Supposons que le nom d'utilisateur est "admin" et le mot de passe est "12345"
+//     if (email === "admin" && password === "12345") {
+//       resolve({ message: "Connexion réussie !" });
+//     } else {
+//       reject({ message: "Nom d'utilisateur ou mot de passe incorrect." });
+//     }
+//   });
+// }
 
-// Fonction pour stocker le token dans le localStorage
-function storeToken(token) {
-  localStorage.setItem("userToken", token);
-}
+
+
 
 // Sélectionner le formulaire et ajouter un gestionnaire d'événement pour la soumission
 const loginForm = document.getElementById("loginForm");
@@ -42,9 +40,9 @@ if (password === '') {
 
 // Si tout est correct, le formulaire peut être soumis
 return true;
-}
+})
 
-fetch('http://localhost:5678/api/users/login', {
+fetch('http://localhost:5678/api/users/login')({
   method: 'POST',
   headers: {
     'Accept': 'application/json',
@@ -66,28 +64,28 @@ fetch('http://localhost:5678/api/users/login', {
       console.log('Utilisateur non trouvé.');
     }
   })
-  .catch((error) => console.log(error));
+  .catch((error) => console.log(error))
 
 
-  // Appeler la fonction de connexion avec les informations d'identification
-  performLogin(username, password)
-    .then((response) => {
-      alert(response.message); // Connexion réussie, affichez un message approprié
+//   // Appeler la fonction de connexion avec les informations d'identification
+//   performLogin(username, password)
+//     .then((response) => {
+//       alert(response.message); // Connexion réussie, affichez un message approprié
       
-      // Supposons que vous avez un jeton d'authentification généré par votre serveur
-      const authToken = "votre_token_d_authentification"; // Remplacez-le par votre jeton réel
+//       // Supposons que vous avez un jeton d'authentification généré par votre serveur
+//       const authToken = "votre_token_d_authentification"; // Remplacez-le par votre jeton réel
 
-      // Stocker le token dans le localStorage
-      storeToken(authToken);
+//       // Stocker le token dans le localStorage
+//       storeToken(authToken);
 
-      // Vous pouvez rediriger l'utilisateur vers une autre page ici
-      // window.location.href = "page_de_destination.html";
-    })
-    .catch((error) => {
-      alert(error.message); // Affichez un message d'erreur en cas d'échec de connexion
-    });
-});
+//       // Vous pouvez rediriger l'utilisateur vers une autre page ici
+//       // window.location.href = "page_de_destination.html";
+//     })
+//     .catch((error) => {
+//       alert(error.message); // Affichez un message d'erreur en cas d'échec de connexion
+//     });
+// });
 
-// Récupérer le token du localStorage (peut être fait à n'importe quel moment dans votre script)
-const storedToken = localStorage.getItem("userToken");
-console.log(storedToken); // Vous pouvez utiliser le token récupéré pour vos actions ultérieures
+// // Récupérer le token du localStorage (peut être fait à n'importe quel moment dans votre script)
+// const storedToken = localStorage.getItem("userToken");
+// console.log(storedToken); // Vous pouvez utiliser le token récupéré pour vos actions ultérieure)
