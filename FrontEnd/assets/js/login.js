0e6c9a3ccefb1ad07b00ae8a222fc21b3824c031
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2023-07-31 16:22:49 +0200

    mode admin

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -22,40 +22,46 @@ loginForm.addEventListener("submit", function (event) {
 
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-  const emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/ 
-//vérifier les champs
-if(email === ''){
-  alert("Veuillez saisir une adresse e-mail.");
-  return false; // Empêcher la soumission du formulaire si le champ est vide
-} else if (!emailRegex.test(email)) {
-  alert("Veuillez saisir une adresse e-mail valide.");
-  return false; // Empêcher la soumission du formulaire si l'adresse e-mail est invalide
-}
+  const emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
-// Vérification du champ "password"
-if (password === '') {
-  alert("Veuillez saisir un mot de passe.");
-  return false; // Empêcher la soumission du formulaire si le champ est vide
-}
+  // Vérifier les champs
+  if (email === '') {
+    alert("Veuillez saisir une adresse e-mail.");
+    return false; // Empêcher la soumission du formulaire si le champ est vide
+  } else if (!emailRegex.test(email)) {
+    alert("Veuillez saisir une adresse e-mail valide.");
+    return false; // Empêcher la soumission du formulaire si l'adresse e-mail est invalide
+  }
 
-// Si tout est correct, le formulaire peut être soumis
-return true;
-})
+  // Vérification du champ "password"
+  if (password === '') {
+    alert("Veuillez saisir un mot de passe.");
+    return false; // Empêcher la soumission du formulaire si le champ est vide
+  }
 
-fetch('http://localhost:5678/api/users/login')({
-  method: 'POST',
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({ email: username, password: password })
-})
+  // Si tout est correct, le formulaire peut être soumis
+  // return true; <- Vous pouvez retirer cette ligne, car il n'est pas nécessaire de retourner true ici
+
+  // Envoi de la requête de connexion au serveur
+  fetch('http://localhost:5678/api/users/login', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email: email, password: password })
+  })
   .then((response) => response.json())
   .then((data) => {
     // Le serveur a renvoyé une réponse au format JSON, nous pouvons accéder aux données ici
     if (data.userFound) {
       // L'utilisateur a été trouvé, enregistrez le jeton dans le stockage local
       localStorage.setItem('token', data.token);
+
+      // Vérifiez si l'utilisateur est un administrateur et activez le mode administrateur si c'est le cas
+      if (isAdmin()) {
+        activerModeAdministrateur();
+      }
 
       // Redirigez vers la page d'accueil (ou toute autre page appropriée)
       window.location.href = '/accueil'; // Remplacez '/accueil' par l'URL de votre page d'accueil
@@ -64,7 +70,22 @@ fetch('http://localhost:5678/api/users/login')({
       console.log('Utilisateur non trouvé.');
     }
   })
-  .catch((error) => console.log(error))
+  .catch((error) => console.log(error));
+});
+
+// Fonction pour vérifier si le token est présent dans le Local Storage et s'il correspond à un administrateur
+function isAdmin() {
+  const token = localStorage.getItem('token');
+  // Remplacez cette condition par celle que vous utilisez pour vérifier si le token est celui d'un administrateur
+  return token !== null && token === 'votre_token_admin'; // Vérifiez si le token est celui d'un administrateur
+}
+
+// Fonction pour activer le mode administrateur
+function activerModeAdministrateur() {
+  // Mettez ici le code pour activer le mode administrateur, par exemple en affichant des fonctionnalités supplémentaires ou en modifiant l'apparence de l'interface.
+  console.log('Mode administrateur activé.');
+}
+
 
 
 //   // Appeler la fonction de connexion avec les informations d'identification
