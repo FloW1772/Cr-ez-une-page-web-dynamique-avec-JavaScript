5357c4cf100c99dfba4e36fa7446085d890ab069
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2023-08-14 18:23:17 +0200

    modale

--- FrontEnd/assets/js/modale.js
+++ FrontEnd/assets/js/modale.js
@@ -1,23 +1,87 @@
-function openModal(imageUrl, title) {
-    const modal = document.getElementById('myModal');
-    const modalImage = modal.querySelector('.modal-image');
-    const modalTitle = modal.querySelector('.modal-title');
-  
-    modalImage.src = imageUrl;
-    modalTitle.textContent = title;
-  
-    modal.style.display = 'block';
-  }
-  // openModalLink.addEventListener('click', () => {
-  //   for (let project of projects) {
-  //     openModal(project.imageUrl, project.title);
-  //   }
-  // });
-  if (isAdmin()){
-    const portfolioTitle  = document.querySelector(".portfolio-title");
-    const modifyBtn = document.createElement("button") 
-    modifyBtn.setAttribute('id', 'modify-btn')
-    modifyBtn.innerHTML = `<i class="fa-regular fa-pen-to-square"></i> <span>modifier</span>`
-    portfolioTitle.appendChild(modifyBtn)
+if (isAdmin()) {
+  // Ajouter le bouton de modification si l'utilisateur est administrateur
+  const portfolioTitle = document.querySelector(".portfolio-title");
+  const modifyBtn = document.createElement("button");
+  const myModal = document.querySelector ('#myModal')
+  const backgroundgray = document.querySelector('.background-gray')
+  modifyBtn.setAttribute('id', 'modify-btn');
+  modifyBtn.innerHTML = `<i class="fa-regular fa-pen-to-square"></i> <span>modifier</span>`;
+  portfolioTitle.appendChild(modifyBtn);
 
+  modifyBtn.addEventListener('click', function(event){
+    myModal.classList.toggle('hidden')
+
+  })
+
+  backgroundgray.addEventListener('click', function(event){
+    myModal.classList.toggle('hidden')
+  })
+  
+  displayGalleryOnModale()
+  // showModal(); // Afficher la modale après avoir ajouté les éléments à la galerie
+}
+
+
+async function displayGalleryOnModale (){
+  // Supposons que ce code soit à l'intérieur de la boucle qui gère l'ouverture de la modale pour chaque projet
+  await getProjects();
+  console.log(projects)
+  const gallerymodal = document.querySelector('.modal-container-gallery')
+  for (let project of projects) {
+      // openModal(project.imageUrl, project.title);
+
+      // Créer une figure pour chaque projet et l'ajouter à la galerie
+      let figure = document.createElement('figure');
+      figure.setAttribute('class', 'display-figure-modal');
+      figure.setAttribute('data-category-id', project.categoryId);
+
+      // Ajouter l'image à l'élément figure
+      let img = document.createElement('img');
+      img.setAttribute('src', project.imageUrl);
+      img.setAttribute('alt', project.title);
+      figure.appendChild(img);
+
+      // Ajouter les informations du projet à l'élément figure
+      let titleElement = document.createElement('p');
+      titleElement.textContent = project.title;
+      figure.appendChild(titleElement);
+
+      // Ajouter la figure à la galerie
+      gallerymodal.appendChild(figure);
+
+      // Créer les boutons cliquables sous les images
+      let button1 = document.createElement('button');
+      button1.textContent = 'Bouton 1';
+      button1.addEventListener('click', () => {
+          // Code à exécuter lorsque le bouton 1 est cliqué
+      });
+      gallerymodal.appendChild(button1);
+
+      let button2 = document.createElement('button');
+      button2.textContent = 'Bouton 2';
+      button2.addEventListener('click', () => {
+          // Code à exécuter lorsque le bouton 2 est cliqué
+          deleteProject(project.id)
+      });
+      gallerymodal.appendChild(button2);
   }
+
+}
+
+function deleteProject (id){
+  const token = localStorage.getItem('token')
+fetch(`http://localhost:5678/api/works/${id}`,{
+  method:"DELETE",
+  headers:{
+    'Content-Type': 'application/json;charset=utf-8',
+    "accept":"*/*",
+    'authorization':`Bearer ${token}`
+  }
+})
+.then((response)=>{
+  console.log(response)
+  displayProjects()
+  displayGalleryOnModale()
+})
+.catch((error)=>console.log(error))
+}
